5de4f29cdb2a03591a00e59b3c07510f60cecb80
Borys Krisko <bvkr@me.com>
2019-09-16 16:48:00 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Борис Крисько on 9/16/19.
+//  Copyright © 2019 Borys Krisko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
