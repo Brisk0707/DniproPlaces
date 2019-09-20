5ce91b27bbc44038eab5ec780be4cbba7c5f6637
Borys Krisko <bvkr@me.com>
2019-09-20 15:21:01 +0300

    working with design

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Борис Крисько on 9/20/19.
+//  Copyright © 2019 Borys Krisko. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameOfPlaceLabel: UILabel!
+    @IBOutlet weak var locationOfPlaceLabel: UILabel!
+    @IBOutlet weak var typeOfPlaceLabel: UILabel!
+
+}
